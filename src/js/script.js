3b146000461e473f7fb37398162bc3cd89eb496b
escowin <edwin@escowinart.com>
2023-08-16 12:51:01 -0500

    captured & displayed copyright year

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 // const { calculator } = require("./lib/Calculator")
 
 // dom elements
+const dateEl = document.getElementById("date");
 const num1El = document.getElementById("num-1");
 const num2El = document.getElementById("num-2");
 const num3El = document.getElementById("num-3")
@@ -8,6 +9,16 @@ const num4El = document.getElementById("num-4")
 const ratioForm = document.getElementById("ratio-form")
 
 // functions
+function displayDate() {
+    let date = new Date().getFullYear()
+    dateEl.innerText = date
+
+    console.log(`
+    \u00A9 ${date} Edwin M. Escobar
+    https://github.com/escowin/ratio-calculator
+    `)
+}
+
 function displayAnswer(e) {
     e.preventDefault()
     console.log(num1El.value)
@@ -17,4 +28,5 @@ function displayAnswer(e) {
 }
 
 // calls
+displayDate()
 ratioForm.addEventListener("submit", (e) => displayAnswer(e))
