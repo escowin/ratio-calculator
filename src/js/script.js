8e9b89be67e9e8223954b90be216b2b186e92e66
escowin <edwin@escowinart.com>
2023-08-17 16:17:28 -0500

    cleared num4 innerText through click event

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,7 @@ const num2El = document.getElementById("num-2");
 const num3El = document.getElementById("num-3");
 const num4El = document.getElementById("num-4");
 const ratioForm = document.getElementById("ratio-form");
+const clearBtn = document.getElementById("clear");
 
 // functions
 function displayDate() {
@@ -25,12 +26,19 @@ function displayAnswer(e) {
   const num1 = num1El.value;
   const num2 = num2El.value;
   const num3 = num3El.value;
-  console.log(num1, num2, num3)
+  console.log(num1, num2, num3);
 
   const num4 = calculator(num1, num2, num3);
   num4El.innerText = num4;
 }
 
+function clearAnswer() {
+  if (num4El.innerText !== "") {
+    num4El.innerText = ""
+  }
+}
+
 // calls
 displayDate();
 ratioForm.addEventListener("submit", (e) => displayAnswer(e));
+clearBtn.addEventListener("click", () => clearAnswer());
