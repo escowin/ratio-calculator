276b0102814716fca3faf55c24e4d0c84f35222b
escowin <edwin@escowinart.com>
2023-08-18 14:00:18 -0500

    reconfigured calculator as OOP class, passing it through TDD

--- __test__/calculator.test.js
+++ __test__/calculator.test.js
@@ -1,10 +1,11 @@
-const { calculator } = require("../src/js/lib/Calculator");
+const Calculator = require("../src/js/lib/Calculator");
 
-test("returns value that is consistent with established ratio input values", () => {
+test("returns missing ratio value", () => {
   const num1 = 16;
   const num2 = 9;
   const num3 = 1024;
-  const num4 = (num1/num2)*num3
+  const num4 = (num2/num1)*num3
+  const calculator = new Calculator(num1, num2, num3)
 
-  expect(calculator(num1, num2, num3)).toEqual(num4);
+  expect(calculator.calculateNum()).toEqual(num4);
 });
--- src/js/lib/Calculator.js
+++ src/js/lib/Calculator.js
@@ -1,5 +1,17 @@
-function calculator(num1, num2, num3) {
-  return (num2/num1)*num3;
+// function calculator(num1, num2, num3) {
+//   return (num2/num1)*num3;
+// }
+
+class Calculator {
+  constructor(num1, num2, num3) {
+    this.num1 = num1;
+    this.num2 = num2;
+    this.num3 = num3;
+  }
+
+  calculateNum() {
+    return (this.num2 / this.num1) * this.num3;
+  }
 }
 
-module.exports = { calculator };
+module.exports = Calculator;
