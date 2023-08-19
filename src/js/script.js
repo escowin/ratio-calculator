3fe467f7695d71c0f7ecd3d20428ff33c9887b27
escowin <edwin@escowinart.com>
2023-08-18 20:59:39 -0500

    cleaned up script from clearing text since p element is no longer used

--- src/js/script.js
+++ src/js/script.js
@@ -5,12 +5,11 @@ const Calculator = require("./lib/Calculator");
 
 // dom elements
 const dateEl = document.getElementById("date");
+const ratioForm = document.getElementById("ratio-form");
 const num1El = document.getElementById("num-1");
 const num2El = document.getElementById("num-2");
 const num3El = document.getElementById("num-3");
 const num4El = document.getElementById("num-4");
-const ratioForm = document.getElementById("ratio-form");
-const clearBtn = document.getElementById("clear");
 
 const numEls = [num1El, num2El, num3El, num4El];
 
@@ -36,19 +35,12 @@ function displayAnswer(e) {
     num3El.value,
     num4El.value
   );
-  
-  // displays the calculated remaining ratio value
-  const emptyEl = numEls.find(el => el.value === "");
-  emptyEl.value = Math.round(calculator.calculateNum());
-}
 
-function clearAnswer() {
-  if (num4El.innerText !== "") {
-    num4El.innerText = "";
-  }
+  // displays the calculated remaining ratio value
+  const emptyEl = numEls.find((el) => el.value === "");
+  emptyEl.value = Math.round(calculator.calculateNum());
 }
 
 // production calls
 displayDate();
 ratioForm.addEventListener("submit", (e) => displayAnswer(e));
-clearBtn.addEventListener("click", () => clearAnswer());
