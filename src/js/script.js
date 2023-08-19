1c82a8c77e5f9c2bdf3966c0add990ac5ffe0b20
escowin <edwin@escowinart.com>
2023-08-18 21:23:47 -0500

    coalated dom elements into array to streamline script, using methods to reduce repetitive code

--- src/js/script.js
+++ src/js/script.js
@@ -6,12 +6,12 @@ const Calculator = require("./lib/Calculator");
 // dom elements
 const dateEl = document.getElementById("date");
 const ratioForm = document.getElementById("ratio-form");
-const num1El = document.getElementById("num-1");
-const num2El = document.getElementById("num-2");
-const num3El = document.getElementById("num-3");
-const num4El = document.getElementById("num-4");
-
-const numEls = [num1El, num2El, num3El, num4El];
+const numEls = [
+  document.getElementById("num-1"),
+  document.getElementById("num-2"),
+  document.getElementById("num-3"),
+  document.getElementById("num-4"),
+];
 
 // functions
 function displayDate() {
@@ -28,13 +28,12 @@ function displayAnswer(e) {
   // prevents input values from disappearing
   e.preventDefault();
 
-  // passes values into instantiated object as constructor arguments
-  const calculator = new Calculator(
-    num1El.value,
-    num2El.value,
-    num3El.value,
-    num4El.value
-  );
+  // extracts, destructures, and assigns values from dom elements
+  const nums = numEls.map((numEl) => numEl.value);
+  const [num1, num2, num3, num4] = nums;
+
+  // creates new object with extracted values
+  const calculator = new Calculator(num1, num2, num3, num4);
 
   // displays the calculated remaining ratio value
   const emptyEl = numEls.find((el) => el.value === "");
