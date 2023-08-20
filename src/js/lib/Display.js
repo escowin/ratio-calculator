2614af1868133d13d8b5dc161fee925a956cd1eb
escowin <edwin@escowinart.com>
2023-08-20 15:56:06 -0500

    moved all dom functions to display class

--- src/js/lib/Display.js
+++ src/js/lib/Display.js
@@ -1,3 +1,5 @@
+const Calculator = require("./Calculator");
+
 class Display {
   constructor() {
     this.year = new Date().getFullYear();
@@ -20,6 +22,19 @@ class Display {
 
   resetWidth(numEls) {
     numEls.forEach((el) => (el.style.width = "1rem"));
+  }
+
+  displayAnswer(e, numEls) {
+    e.preventDefault();
+    // extracts, destructures, and assigns values from dom elements to instantiated object
+    const nums = numEls.map((numEl) => numEl.value);
+    const [num1, num2, num3, num4] = nums;
+    const calculator = new Calculator(num1, num2, num3, num4);
+
+    // displays the calculated remaining ratio value
+    const emptyEl = numEls.find((el) => el.value === "");
+    emptyEl.value = Math.round(calculator.calculateNum());
+    emptyEl.style.width = `${emptyEl.value.length}rem`;
   }
 }
 
--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,6 @@
 import "../css/style.css";
 
 // global variables
-const Calculator = require("./lib/Calculator");
 const Display = require("./lib/Display");
 const clearBtn = document.getElementById("clear");
 const ratioForm = document.getElementById("ratio-form");
@@ -14,22 +13,8 @@ for (let i = 1; i <= 4; i++) {
 const display = new Display();
 display.displayDate();
 
-// functions
-function displayAnswer(e) {
-  e.preventDefault();
-  // extracts, destructures, and assigns values from dom elements to instantiated object
-  const nums = numEls.map((numEl) => numEl.value);
-  const [num1, num2, num3, num4] = nums;
-  const calculator = new Calculator(num1, num2, num3, num4);
-
-  // displays the calculated remaining ratio value
-  const emptyEl = numEls.find((el) => el.value === "");
-  emptyEl.value = Math.round(calculator.calculateNum());
-  emptyEl.style.width = `${emptyEl.value.length}rem`;
-}
-
-// calls
-ratioForm.addEventListener("submit", (e) => displayAnswer(e));
+// event listeners
+ratioForm.addEventListener("submit", (e) => display.displayAnswer(e, numEls));
 clearBtn.addEventListener("click", () => display.resetWidth(numEls));
 numEls.forEach((el) => {
   el.addEventListener("input", (el) => display.adjustWidth(el.target))
