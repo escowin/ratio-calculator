6b628fb38995d16bcb6d655b426ef966d265fe4b
escowin <edwin@escowinart.com>
2023-08-20 15:41:40 -0500

    modularized script dom functions into Display class

--- src/js/lib/Display.js
+++ src/js/lib/Display.js
@@ -1,9 +1,5 @@
-const Calculator = require("./Calculator");
-
-class Display extends Calculator {
-  constructor(num1, num2, num3, num4) {
-    // inherited Calculator properties
-    super(num1, num2, num3, num4);
+class Display {
+  constructor() {
     this.year = new Date().getFullYear();
   }
 
@@ -15,6 +11,15 @@ class Display extends Calculator {
     \u00A9 ${this.year} Edwin M. Escobar
     https://github.com/escowin/ratio-calculator
     `);
+  }
+
+  adjustWidth(numEl) {
+    let num = numEl.value.length;
+    numEl.style.width = `${num}rem`;
+  }
+
+  resetWidth(numEls) {
+    numEls.forEach((el) => (el.style.width = "1rem"));
   }
 }
 
--- src/js/script.js
+++ src/js/script.js
@@ -4,22 +4,17 @@ import "../css/style.css";
 // global variables
 const Calculator = require("./lib/Calculator");
 const Display = require("./lib/Display");
-const clearBtn = document.getElementById("clear")
+const clearBtn = document.getElementById("clear");
 const ratioForm = document.getElementById("ratio-form");
 const numEls = [];
 for (let i = 1; i <= 4; i++) {
   numEls.push(document.getElementById(`num-${i}`));
 }
 
-const display = new Display()
-display.displayDate()
+const display = new Display();
+display.displayDate();
 
 // functions
-function adjustWidth(numEl) {
-  let num = numEl.value.length;
-  numEl.style.width = `${num}rem`;
-}
-
 function displayAnswer(e) {
   e.preventDefault();
   // extracts, destructures, and assigns values from dom elements to instantiated object
@@ -30,14 +25,12 @@ function displayAnswer(e) {
   // displays the calculated remaining ratio value
   const emptyEl = numEls.find((el) => el.value === "");
   emptyEl.value = Math.round(calculator.calculateNum());
-  emptyEl.style.width = `${emptyEl.value.length}rem`
-}
-
-function resetWidth() {
-  numEls.forEach(el => el.style.width = "1rem")
+  emptyEl.style.width = `${emptyEl.value.length}rem`;
 }
 
 // calls
-numEls.forEach((el) => el.addEventListener("input", (el) => adjustWidth(el.target)));
 ratioForm.addEventListener("submit", (e) => displayAnswer(e));
-clearBtn.addEventListener("click", () => resetWidth())
+clearBtn.addEventListener("click", () => display.resetWidth(numEls));
+numEls.forEach((el) => {
+  el.addEventListener("input", (el) => display.adjustWidth(el.target))
+});
