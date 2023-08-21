d098e72637dad653829bc45b5ca7be1c4b759a16
escowin <edwin@escowinart.com>
2023-08-20 20:57:46 -0500

    reconfigured display test w/ note

--- __test__/Display.test.js
+++ __test__/Display.test.js
@@ -3,17 +3,13 @@
  */
 const Display = require("../src/js/lib/Display");
 
-test("object gets the current date", () => {
-  const display = new Display();
-  const year = new Date().getFullYear()
-  
-  expect(display.year).toEqual(year)
-})
-
 test("object contains DOM elements", () => {
   const display = new Display();
 
+  expect(display.dateEl).toBeDefined()
   expect(display.clearBtn).toBeDefined()
   expect(display.form).toBeDefined()
   expect(display.numEls).toBeDefined()
 })
+
+// to-do: set up mock dom to pass dom methods through tdd
