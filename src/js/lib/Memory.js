02a270fd0858876eca0512293be8ee5e4302ffaa
escowin <edwin@escowinart.com>
2023-08-23 22:51:26 -0500

    set up indexedb db transaction & indexes

--- src/js/lib/Memory.js
+++ src/js/lib/Memory.js
@@ -40,10 +40,23 @@ class Memory {
     let db;
     const request = window.indexedDB.open("ratioCalculator", 1);
 
-    request.onerror = (e) => console.error("error " + e);
-    request.onsuccess = (e) => {
+    request.onerror = (e) => console.error(e.target.errorCode);
+    // request.onsuccess = (e) => (db = e.target.result);
+    request.onupgradeneeded = (e) => {
       db = e.target.result;
-      console.log(db)
+      const store = db.createObjectStore("ratios", { autoIncrement: true });
+      // for (let i = 1; i < 5; i++) {
+      //   store.createIndex(`num${i}`, `num${i}`, { unique: false });
+      // }
+      console.log(db);
+      console.log(store);
+
+      store.transaction.oncomplete = (e) => {
+        const ratioStore = db
+          .transaction("ratios", "readwrite")
+          .objectStore("ratios");
+        console.log(ratioStore);
+      };
     };
   }
 }
